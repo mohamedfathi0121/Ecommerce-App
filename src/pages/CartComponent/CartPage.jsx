02abc9815893fcce25b8dd59a115209a04f7d084
Mohamed Sherif <142669522+Mohamed6Sherif@users.noreply.github.com>
2025-06-05 18:15:00 +0300

    new push

--- src/pages/CartComponent/CartPage.jsx
+++ src/pages/CartComponent/CartPage.jsx
@@ -1,22 +1,22 @@
 import React, { PureComponent } from "react";
-import "./CartPage.css";
+import style from "./CartPage.module.css";
 
 export class CartPage extends PureComponent {
   render() {
     return (
-      <div class="container">
-        <h1 className="cart-title">Your Cart</h1>
-        <div className="cart_div">
+      <div class={style.container}>
+        <h1 className={style.cartTitle}>Your Cart</h1>
+        <div className={style.cartDiv}>
           {/* Right Section Cart-Items */}
-          <div className="cart-container">
+          <div className={style.cartContainer}>
             {/* item 1 */}
-            <div className="cart-item">
+            <div className={style.cartItem}>
               <img
-                className="item-image"
+                className={style.itemImage}
                 src="/src/assets/jacket.png"
                 alt="Gradient Graphic T-shirt"
               />
-              <div className="item-details">
+              <div className={style.itemDetails}>
                 <h3>Gradient Graphic T-shirt</h3>
                 <p>
                   <span>Size: </span>Large
@@ -24,10 +24,10 @@ export class CartPage extends PureComponent {
                 <p>
                   <span>Color: </span>Yellow
                 </p>
-                <h6 className="item-price">$145</h6>
+                <h6 className={style.itemPrice}>$145</h6>
               </div>
-              <div className="quantity-control">
-                <div className="item-amount">
+              <div className={style.quantityControl}>
+                <div className={style.itemAmount}>
                   <input
                     type="number"
                     min="1"
@@ -35,69 +35,7 @@ export class CartPage extends PureComponent {
                     name="number_of_item"
                   />
                 </div>
-                <div className="delete-icon">
-                  <img src="/src/assets/delete.png" alt="delete" />
-                </div>
-              </div>
-            </div>
-            {/* item 2 */}
-            <div className="cart-item">
-              <img
-                className="item-image"
-                src="/src/assets/jacket.png"
-                alt="Gradient Graphic T-shirt"
-              />
-              <div className="item-details">
-                <h3>Gradient Graphic T-shirt</h3>
-                <p>
-                  <span>Size: </span>Large
-                </p>
-                <p>
-                  <span>Color: </span>Yellow
-                </p>
-                <h6 className="item-price">$145</h6>
-              </div>
-              <div className="quantity-control">
-                <div className="item-amount">
-                  <input
-                    type="number"
-                    min="1"
-                    value="1"
-                    name="number_of_item"
-                  />
-                </div>
-                <div className="delete-icon">
-                  <img src="/src/assets/delete.png" alt="delete" />
-                </div>
-              </div>
-            </div>
-            {/* item 3 */}
-            <div className="cart-item">
-              <img
-                className="item-image"
-                src="/src/assets/jacket.png"
-                alt="Gradient Graphic T-shirt"
-              />
-              <div className="item-details">
-                <h3>Gradient Graphic T-shirt</h3>
-                <p>
-                  <span>Size: </span>Large
-                </p>
-                <p>
-                  <span>Color: </span>Yellow
-                </p>
-                <h6 className="item-price">$145</h6>
-              </div>
-              <div className="quantity-control">
-                <div className="item-amount">
-                  <input
-                    type="number"
-                    min="1"
-                    value="1"
-                    name="number_of_item"
-                  />
-                </div>
-                <div className="delete-icon">
+                <div className={style.deleteIcon}>
                   <img src="/src/assets/delete.png" alt="delete" />
                 </div>
               </div>
@@ -105,30 +43,30 @@ export class CartPage extends PureComponent {
           </div>
 
           {/* Left Section order-summary */}
-          <div className="order-summary_right">
+          <div className={style.orderSummaryRight}>
             <h2>Order Summary</h2>
-            <div className="summary-item">
+            <div className={style.summaryItem}>
               <span>Subtotal</span>
               <span>$565</span>
             </div>
-            <div className="summary-item">
+            <div className={style.summaryItem}>
               <span>Discount (-20%)</span>
-              <span className="discount">-$113</span>
+              <span className={style.discount}>-$113</span>
             </div>
-            <div className="summary-item">
+            <div className={style.summaryItem}>
               <span>Delivery Fee</span>
               <span>$15</span>
             </div>
-            <div className="summary-item total">
+            <div className={style.summaryItem} {...style.total}>
               <span>Total</span>
               <span>$467</span>
             </div>
 
-            <div className="promo">
+            <div className={style.promo}>
               <input type="text" placeholder="&#127873;  Add promo code" />
               <button type="button">Apply</button>
             </div>
-            <a href="#" className="checkout-button">
+            <a href="#" className={style.checkoutButton}>
               Go to Checkout &rarr;
             </a>
           </div>
